8a449a6a68a65f5cb3ac15e3b7c3b8f2608fea1f
Mahammad Yagubbayli <ymahammad@outlook.com>
2022-11-21 11:23:42 +0300

    Updated version (Prime Finder)

--- java_beginner/prime_numbers_finder/src/Main.java
+++ java_beginner/prime_numbers_finder/src/Main.java
@@ -1,7 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-
-
         for (int i = 1; i < 101; i++) {
             int number = 0;
             for(int j = 2; j < 101; j++){
@@ -13,6 +11,5 @@ public class Main {
                 System.out.print(i + " ");
             }
         }
-
     }
 }
