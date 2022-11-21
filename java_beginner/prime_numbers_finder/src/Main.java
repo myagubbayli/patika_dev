9ff1004ec600aa1ba6012701f0c2fa40ddf5b17c
Mahammad Yagubbayli <ymahammad@outlook.com>
2022-11-21 11:22:42 +0300

    Prime Number Finder

---
+++ java_beginner/prime_numbers_finder/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        for (int i = 1; i < 101; i++) {
+            int number = 0;
+            for(int j = 2; j < 101; j++){
+                if (i % j == 0) {
+                    number++;
+                }
+            }
+            if(number == 1) {
+                System.out.print(i + " ");
+            }
+        }
+
+    }
+}
