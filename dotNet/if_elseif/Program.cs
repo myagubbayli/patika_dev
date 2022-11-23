cc0e519af76eab90b15b6e8d1983eeb8d702190d
Mahammad Yagubbayli <ymahammad@outlook.com>
2022-11-23 18:18:01 +0300

    If Else if

---
+++ dotNet/if_elseif/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program {
+    static void Main(string[] args) {
+        int time = DateTime.Now.Hour;
+        System.Console.WriteLine(time);
+
+        if (time >= 6 && time < 11) 
+            System.Console.WriteLine("Good Morning!");
+        else if (time <= 18)
+            System.Console.WriteLine("Good days!");
+        else 
+            System.Console.WriteLine("Good nights!");
+
+        string result = time <= 18 ? "Good days!" : "Good night!";
+
+        result = time >= 6 && time < 11 ? "Good Morning!" : time <= 18 ? "Good Days!" : "Good night!";
+
+        System.Console.WriteLine(result);
+    }
+}
