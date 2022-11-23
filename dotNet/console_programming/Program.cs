7945ed23f5d589e7cc28b24bd3bc59f6aa4567ca
Mahammad Yagubbayli <ymahammad@outlook.com>
2022-11-23 15:05:47 +0300

    My first dotNet notes

---
+++ dotNet/console_programming/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace console_programming {
+    class Program {
+        public static void Main(string[] args) {
+            Console.WriteLine("Hello world!");
+            Console.WriteLine("Give your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Give your surname: ");
+            string surname = Console.ReadLine();
+
+            Console.WriteLine("Hello " + name + " " + surname);
+        }
+    }
+}
---
+++ dotNet/variables/Program.cs
@@ -0,0 +1,21 @@
+﻿// Variable conversions
+string str20 = "20";
+int int20 = 20;
+string newValue = str20 + int20.ToString();
+Console.WriteLine(newValue); // Output 2020
+
+int int21 = int20 + Convert.ToInt32(str20);
+Console.WriteLine(int21); // Output 40
+
+int int22 = int20 + int.Parse(str20); // Output 40
+
+// Datetime
+string datetime = DateTime.Now.ToString("dd.MM.yyyy");
+Console.WriteLine(datetime);
+
+string datetime2 = DateTime.Now.ToString("dd/MM/yyyy");
+Console.WriteLine(datetime2);
+
+// time
+string hour = DateTime.Now.ToString("HH:mm");
+Console.WriteLine(hour);
