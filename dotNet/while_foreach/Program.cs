019a8d23ff7212f0270f1f14777e2550335862db
Mahammad Yagubbayli <ymahammad@outlook.com>
2022-11-23 21:53:45 +0300

    while_foreach

---
+++ dotNet/while_foreach/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program {
+    static void Main(string[] args) {
+        System.Console.Write("Enter number: ");
+        int number = int.Parse(Console.ReadLine());
+        int counter = 1;
+        int total = 0;
+
+        while (counter <= number) {
+            total += counter;
+            counter++;
+        }
+        System.Console.WriteLine(total/number);
+        
+        char character = 'a';
+        while(character < 'z') {
+            System.Console.Write(character);
+            character++;
+        }
+        System.Console.WriteLine();
+
+        System.Console.WriteLine("****** FOREACH ******");
+        string[] cars = {"BMW", "Ford", "Toyota", "Nissan"};
+
+        foreach (var car in cars) {
+            System.Console.WriteLine(car);
+        }
+    }
+}
