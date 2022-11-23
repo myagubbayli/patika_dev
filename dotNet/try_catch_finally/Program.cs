9208ce7d64e5a2dcdb3ce655b9ce3987de4a7e6b
Mahammad Yagubbayli <ymahammad@outlook.com>
2022-11-23 18:07:13 +0300

    Try catch finally

---
+++ dotNet/try_catch_finally/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+class Solution {
+    static void Main(String[] args) {
+        try {
+            int a = int.Parse(Console.ReadLine());
+        
+            int b = int.Parse(Console.ReadLine());
+        
+            int c = a+b;
+        
+            Console.WriteLine(c);
+        } catch(Exception ex) {
+            Console.WriteLine("Bir Hata Oluştu: "+ ex.Message);
+        } finally {
+            Console.WriteLine("İşlem tamamlandı.");
+        }
+    }
+}      
+
+
+
+
