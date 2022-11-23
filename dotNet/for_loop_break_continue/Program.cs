abff48d513021e0fdd6c4c484655aa33cf3ca887
Mahammad Yagubbayli <ymahammad@outlook.com>
2022-11-23 19:58:52 +0300

    staircase_exercise

--- dotNet/for_loop_break_continue/Program.cs
+++ dotNet/for_loop_break_continue/Program.cs
@@ -2,26 +2,26 @@
 
 class Program {
     static void Main(string[] args) {
-        //System.Console.WriteLine("Please give a number: ");
-        //int counter = int.Parse(Console.ReadLine());
-//
-        //for (int i = 1; i <= counter; i++) {
+        // System.Console.WriteLine("Please give a number: ");
+        // int counter = int.Parse(Console.ReadLine());
+
+        // for (int i = 1; i <= counter; i++) {
         //    if (i % 2 == 1) 
         //        System.Console.WriteLine(i);
-        //}
-//
-        //int oddTotal = 0;
-        //int evenTotal = 0;
-//
-        //for (int i = 1; i <= 1000; i++) {
+        // }
+
+        // int oddTotal = 0;
+        // int evenTotal = 0;
+
+        // for (int i = 1; i <= 1000; i++) {
         //    if (i % 2 == 1)
         //        oddTotal += i;
         //    else 
         //        evenTotal += i;
-        //}
-//
-        //System.Console.WriteLine("Odd total: " + oddTotal);
-        //System.Console.WriteLine("Even total: " + evenTotal);
+        // }
+
+        // System.Console.WriteLine("Odd total: " + oddTotal);
+        // System.Console.WriteLine("Even total: " + evenTotal);
 
         for (int i = 1; i < 10; i++) {
             if (i == 4) 
