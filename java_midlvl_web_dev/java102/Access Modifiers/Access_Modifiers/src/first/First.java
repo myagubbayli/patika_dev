065682c90e24ac6e3f2aeff285f677d27b05bb90
Mahammad Yagubbayli <ymahammad@outlook.com>
2022-12-02 13:21:21 +0300

    Access Modifiers

---
+++ java_midlvl_web_dev/java102/Access Modifiers/Access_Modifiers/src/first/First.java
@@ -0,0 +1,14 @@
+package first;
+
+public class First {
+    public String str;
+
+    public First (String str) {
+        this.str = str;
+    }
+
+    public void show() {
+        System.out.println(this.str);
+    }
+
+}
---
+++ java_midlvl_web_dev/java102/Access Modifiers/Access_Modifiers/src/first/Main.java
@@ -0,0 +1,8 @@
+package first;
+
+public class Main {
+    public static void main(String[] args) {
+        First a = new First("Hello world");
+        System.out.println(a.str);
+    }
+}
