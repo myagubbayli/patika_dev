09c5263d5da0419cb8d434c21db17a8956c27104
Mahammad Yagubbayli <ymahammad@outlook.com>
2022-12-10 20:17:01 +0300

    Delete java_midlvl_web_dev/java102/Nested Classes & Generic Structures/Nested Classes/src directory

--- java_midlvl_web_dev/java102/Nested Classes & Generic Structures/Nested Classes/src/examples/Example3.java
+++
@@ -1,9 +0,0 @@
-package src.examples;
-
-public class Example3 {
-    static class Inner {
-        public void run() {
-            System.out.println("The method of the inner class worked");
-        }
-    }
-}
--- java_midlvl_web_dev/java102/Nested Classes & Generic Structures/Nested Classes/src/nested/Anonymous.java
+++
@@ -1,9 +0,0 @@
-package src.nested;
-
-public class Anonymous {
-
-    public void run() {
-        System.out.println("Main Anonymous class worked!");
-    }
-
-}
