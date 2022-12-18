0a02423813a9684870d9a2f0bc3bd23f23004e00
Mahammad Yagubbayli <ymahammad@outlook.com>
2022-12-18 13:34:32 +0300

    Stop and Pause in Threads

---
+++ java_midlvl_web_dev/java102/MultiThreads and Concurrency/Thread - Stop and Pause/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        Counter c1 = new Counter("c1", 1);
+        Thread t1 = new Thread(c1);
+
+        t1.start();
+        Thread.sleep(1000);
+        c1.stop();
+    }
+}
